862ee47538dcf7dfba771248f66ec4b9d705442a
yfdd617235 <yosefdavid91@gmail.com>
2025-05-10 18:53:14 -0500

    base

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/museoPE/' // Esto es importante para que las rutas se resuelvan correctamente en GitHub Pages
+  // base: '/museoPE/'  Esto es importante para que las rutas se resuelvan correctamente en GitHub Pages
+  base: '',
+
 });
